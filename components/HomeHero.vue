8249fb68ad0fcf50952a3ecfa5809d397b50f114
Maximilian Flechtner <66118057+MaximilianFlechtner@users.noreply.github.com>
2022-01-25 19:35:50 +0100

    *nuxt-img

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -71,7 +71,7 @@
     <div
       class="lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 w-full mt-10 lg:mt-0"
     >
-      <img
+      <nuxt-img
         class="
           h-56
           w-full
@@ -85,6 +85,10 @@
         alt="Maximilian Flechtner"
         height="100vh"
         width="100vw"
+        sizes="xs:20vw sm:20vw md:50vw lg:100vw"
+        quality="90"
+        format="webp"
+        preload
       />
     </div>
   </div>
